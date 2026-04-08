08c44ecdfef354e262c6b1b3ecb0ed3a21c8a74f
Shubham <shub@murphy.local>
2026-04-09 01:57:26 +0530

    Added Delete and patch api endpoints completed the crud api

--- day6/app.js
+++ day6/app.js
@@ -50,6 +50,81 @@ app.post("/api/v1/products", async (req, res) => {
   });
 });
 
+app.patch("/api/v1/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const data = req.body;
+
+  const products = await myReadFile("./data.json");
+  const idx = products.findIndex((elem) => {
+    return elem.id === productId;
+  });
+
+  if (idx === -1) {
+    res.status(400);
+    res.json({
+      isSuccess: false,
+      message: "Invalid Product id",
+    });
+    return;
+  }
+  const odlObj = products[idx];
+  // basically whatever is new in data will be replaced
+  // in the oldObj
+  products[idx] = { ...odlObj, ...data };
+  await mySaveFile("./data.json", products);
+  res.status(204);
+  res.json({
+    isSuccess: true,
+    message: "Product is added to db",
+    data: {
+      product: products[idx],
+    },
+  });
+});
+//
+app.delete("/api/v1/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const products = await myReadFile("./data.json");
+  // const newproducts = [];
+  // products.forEach((element) => {
+  //   if (element.id != productId) newproducts.push(element);
+  // });
+  // await mySaveFile("./data.json", newproducts);
+  // res.status(200);
+  // res.json({
+  //   isSuccess: true,
+  //   message: "Product deleted",
+  //   data: {
+  //     products: {
+  //       newproducts,
+  //     },
+  //   },
+  // });
+  // better approach
+  const idx = products.findIndex((element) => {
+    return element.id === productId;
+  });
+  if (idx === -1) {
+    res.status(400);
+    res.json({
+      isSuccess: false,
+      message: "Invalid Product id",
+    });
+    return;
+  }
+  products.splice(idx, 1);
+  await mySaveFile("./data.json", products);
+  res.status(200);
+  res.json({
+    isSuccess: true,
+    message: "Product deleted",
+    data: {
+      products: {
+        products,
+      },
+    },
+  });
+});
 app.listen(3000, () => {
   console.log("App is listning at port 3000");
 });
