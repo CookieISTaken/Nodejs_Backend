290a8e8ed51b5c5b6481f1aa66b255bb69d08665
Shubham <shub@murphy.local>
2026-04-11 03:40:15 +0530

    created CRUD api endpoints for orders

--- day7/api/v1/orders/routes.js
+++ day7/api/v1/orders/routes.js
@@ -1,21 +1,134 @@
 const express = require("express");
+const { myReadFile, mySaveFile } = require("../../../utils/file_helper");
+const { v4: uuidv4 } = require("uuid");
 
 const ordersRouter = express.Router();
 
-ordersRouter.get("/", (req, res) => {
-  res.status(200).send("(GET) orders dummy response");
+ordersRouter.get("/", async (req, res) => {
+  const orderlist = await myReadFile("./orders.json");
+  res.status(200).json({
+    isSuccess: true,
+    data: {
+      orderlist,
+    },
+  });
 });
 
-ordersRouter.post("/", (req, res) => {
-  res.status(200).send("(POST) orders dummy response");
+ordersRouter.post("/:prodId", async (req, res) => {
+  const { prodId } = req.params;
+  const { quantity } = req.body;
+  const productsList = await myReadFile("./products.json");
+  const idx = productsList.findIndex((elem) => {
+    return elem.id === prodId;
+  });
+  if (idx === -1) {
+    res.status(409).json({
+      isSuccess: false,
+      message: "Invalid Product id!",
+    });
+  } else {
+    if (productsList[idx].quantity == 0) {
+      res.status(409).json({
+        isSuccess: false,
+        message: "Prodcut out of stock!",
+      });
+    } else {
+      const currentprod = productsList[idx];
+      productsList[idx] = {
+        ...currentprod,
+        quantity: currentprod.quantity - quantity,
+      };
+      await mySaveFile("./products.json", productsList);
+      const orderList = await myReadFile("./orders.json");
+      const neworder = {
+        prodId: prodId,
+        quantity: quantity,
+        id: uuidv4(),
+      };
+      orderList.push(neworder);
+      await mySaveFile("./orders.json", orderList);
+      res.status(200).json({
+        isSuccess: true,
+        message: "Order Confirm",
+        order: {
+          name: productsList[idx].name,
+          quantity: quantity,
+        },
+      });
+    }
+  }
 });
 
-ordersRouter.patch("/", (req, res) => {
-  res.status(200).send("(POST) orders dummy response");
+ordersRouter.patch("/:orderId", async (req, res) => {
+  const { orderId } = req.params;
+  const { newquantity } = req.body;
+  const orderList = await myReadFile("./orders.json");
+  const idx = orderList.findIndex((elem) => {
+    return elem.id === orderId;
+  });
+  console.log("Code running till here!!");
+  if (idx === -1) {
+    res.status(404).json({
+      isSuccess: false,
+      message: "Invalid order id!",
+    });
+  } else {
+    const extraprod = orderList[idx].quantity - newquantity;
+    console.log("Code running till here 2!!");
+    const productsList = await myReadFile("./products.json");
+    const prodId = orderList[idx].prodId;
+    const prodIdx = productsList.findIndex((elem) => {
+      return elem.id === prodId;
+    });
+    const oldProd = productsList[prodIdx];
+    productsList[prodIdx] = {
+      ...oldProd,
+      quantity: oldProd.quantity + extraprod,
+    };
+    console.log("Code running till here 3!!");
+    await mySaveFile("./products.json", productsList);
+
+    const oldOrder = orderList[idx];
+    orderList[idx] = { ...oldOrder, quantity: newquantity };
+    await mySaveFile("./orders.json", orderList);
+    res.status(200).json({
+      isSuccess: true,
+      message: "Order modified",
+    });
+  }
 });
 
-ordersRouter.delete("/", (req, res) => {
-  res.status(200).send("(DELETE) orders dummy response");
+ordersRouter.delete("/:orderId", async (req, res) => {
+  const { orderId } = req.params;
+  const ordersList = await myReadFile("./orders.json");
+  const idx = ordersList.findIndex((elem) => {
+    return elem.id === orderId;
+  });
+  if (idx === -1) {
+    res.status(404).json({
+      isSuccess: false,
+      message: "Invalid order id!",
+    });
+  } else {
+    const prodId = ordersList[idx].prodId;
+    const orderQuatity = ordersList[idx].quantity;
+    const productsList = await myReadFile("./products.json");
+    const prodIdx = productsList.findIndex((elem) => {
+      return elem.id === prodId;
+    });
+    const oldProd = productsList[prodIdx];
+    productsList[prodIdx] = {
+      ...oldProd,
+      quantity: productsList[prodIdx].quantity + orderQuatity,
+    };
+    await mySaveFile("./products.json", productsList);
+    ordersList.splice(idx, 1);
+    await mySaveFile("./orders.json", ordersList);
+    res.status(200).json({
+      isSuccess: true,
+      message: "Order deleted",
+    });
+  }
 });
 
 module.exports = { ordersRouter };
